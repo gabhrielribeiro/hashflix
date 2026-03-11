6def97ab71d359547a9f418fe7cfb834ee623dd8
gabhrielribeiro <gabhrielribeiro@gmail.com>
2026-03-10 22:00:53 -0300

    app

--- filmes/apps.py
+++ filmes/apps.py
@@ -4,3 +4,21 @@ from django.apps import AppConfig
 class FilmesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filmes'
+
+    def ready(self):
+    from contas.models import Usuario
+    import os
+
+    email = os.getenv("EMAIL_ADMIN")
+    senha = os.getenv("SENHA_ADMIN")
+
+    usuarios = Usuario.objects.filter(email=email)
+
+    if not usuarios:
+        Usuario.objects.create_superuser(
+            username="admin",
+            email=email,
+            password=senha,
+            is_active=True,
+            is_staff=True
+        )
